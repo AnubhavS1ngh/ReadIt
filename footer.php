7a90602d2894786fc0e0ad481a9942cd2d785e40
AnubhavS1ngh <77298442+AnubhavS1ngh@users.noreply.github.com>
2024-07-30 08:36:18 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,41 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <a href="gnikanubhav771@gmail.com"> <i class= "fas fa-envelope"></i>gnikanubhav771@gmail.com</a>
+         <p> <i class="fas fa-map-marker-alt"></i> New Delhi,India - 110077 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="https://www.instagram.com/_xp1__?igsh=MWtnbWZza25vMGpiYg=="> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="https://www.linkedin.com/in/anubhav-kumar-singh-xp1?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"> <i class="fab fa-linkedin"></i> LinkedIn </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> </p>
+
+</section>
